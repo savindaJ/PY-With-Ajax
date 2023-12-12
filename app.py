a17b89503972a1f9351af0c234c991304029857e
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2023-12-12 10:47:41 +0530

    add annotation !

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/test', methods='POST')
+def test_post():
+    print(request.form)
+
+
 if __name__ == '__main__':
     app.run(
         host='127.0.0.1', port=5000, debug=True
