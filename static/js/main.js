b58beca0f96be8d6245eab403165c476aaa247f7
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2023-12-12 11:14:36 +0530

    add components

--- static/js/main.js
+++ static/js/main.js
@@ -20,3 +20,49 @@ $('#btnSave').on('click',function () {
         }
     })
 });
+
+$('#btnSave').on('click',function () {
+    $.ajax({
+        url:"/test",
+        type:"POST",
+        data:{
+            cusId: $('#cusId').val(),
+            name: $('#name').val(),
+            address: $('#address').val(),
+            salary: $('#salary').val()
+        },
+        success:function (res) {
+            if (res.state==='200'){
+                alert("successfully saved !")
+            }else {
+                alert("bad request !")
+            }
+        },
+        error:function (err) {
+
+        }
+    })
+});
+
+$('#btnSave').on('click',function () {
+    $.ajax({
+        url:"/test",
+        type:"POST",
+        data:{
+            cusId: $('#cusId').val(),
+            name: $('#name').val(),
+            address: $('#address').val(),
+            salary: $('#salary').val()
+        },
+        success:function (res) {
+            if (res.state==='200'){
+                alert("successfully saved !")
+            }else {
+                alert("bad request !")
+            }
+        },
+        error:function (err) {
+
+        }
+    })
+});
