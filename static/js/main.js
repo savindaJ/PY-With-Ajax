069fc83a4eb281694c72c5335260d1edfc13c326
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2023-12-12 10:46:44 +0530

    eclere ajax

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,14 @@
 $('#btnSave').on('click',function () {
+    $.ajax({
+        url:"/test",
+        type:'POST',
+        contentType:"application/json",
+        data:{},
+        success:function (res) {
 
+        },
+        error:function (err) {
+
+        }
+    })
 });
